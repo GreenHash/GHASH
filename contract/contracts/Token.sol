682335307686f1fe548559242b3f507b705c3ba7
GreenHash <87408613+GreenHash@users.noreply.github.com>
2021-07-23 03:52:59 +0200

    Delete contract directory

--- contract/contracts/Token.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.4.19;
-    import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
-
-    contract Token is MintableToken {
-        string public name = "GreenHash";
-        string public symbol = "GHASH";
-        uint8 public decimals = 18;
-    }
